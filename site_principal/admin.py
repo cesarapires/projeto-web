b55aebb8252e2abf995430768d065e5efa23fbe5
Cesar Pires <cesar.augusto.pires@hotmail.com>
2025-10-23 17:56:50 -0300

    Adiciona modelo Usuario ao registro do admin e atualiza importações

--- site_principal/admin.py
+++ site_principal/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from .models import  Livro, Autor, Cliente, Emprestimo, ItemEmprestimo
+from .models import  Livro, Autor, Cliente, Emprestimo, ItemEmprestimo, Usuario
 
 admin.site.register(Livro)
 admin.site.register(Autor)
 admin.site.register(Cliente)
 admin.site.register(Emprestimo)
 admin.site.register(ItemEmprestimo)
+admin.site.register(Usuario)
 
--- site_principal/models/__init__.py
+++ site_principal/models/__init__.py
@@ -3,3 +3,4 @@ from .livro import Livro
 from .cliente import Cliente
 from .emprestimo import Emprestimo
 from .item_emprestimo import ItemEmprestimo
+from .usuario import Usuario
